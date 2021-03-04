7a52b2a385ae3267c772065ba5aed0ee48e90eee
zexoverz <aryuical@gmail.com>
2021-03-04 11:22:21 +0700

    Registration User + testing feature

---
+++ app/contracts/Registration.sol
@@ -0,0 +1,40 @@
+contract Registration {
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    struct User {
+        string name;
+        uint8 age;
+        bool registered;
+    }
+
+    event NewUser(address userAddress, string name, uint8 age);
+    event NewValue(address userAdress, uint256 value);
+
+    mapping(address => User) public users;
+
+    /// modifier to allow only owner to register new user
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only owner can register a user.");
+        _;
+    }
+
+    function registerUser(
+        address _address,
+        string memory _name,
+        uint8 _age
+    ) public onlyOwner {
+        users[_address] = User(_name, _age, true);
+    }
+
+    function send(uint256 _value) public {
+        require(
+            users[msg.sender].registered == true,
+            "Caller is not registered."
+        );
+        emit NewValue(msg.sender, _value);
+    }
+}
---
+++ app/migrations/2_deploy_contracts.js
@@ -0,0 +1,38 @@
+const Registration = artifacts.require('Registration')
+const fs = require('fs')
+
+module.exports = function (deployer) {
+// Use deployer to state migration tasks.
+  deployer.deploy(Registration).then(() => {
+    fs.writeFile(
+        __dirname + path,
+        'const ADDRESS = ' + "'" + Registration.address + "';",
+        (err) => {
+          if (err) {
+            console.log(err)
+          } else {
+          }
+        },
+    )
+  
+    fs.appendFile(
+        __dirname + path,
+        '\nconst ABI = ' + JSON.stringify(Registration.abi) + ';',
+        (err) => {
+          if (err) {
+            console.log(err)
+          } else {
+            fs.appendFile(
+              __dirname + path,
+              '\nmodule.exports = { ADDRESS, ABI };',
+              (err) => {
+                if (err) {
+                  console.log(err)
+                }
+              },
+            )
+          }
+        },
+    )  
+  })
+}
